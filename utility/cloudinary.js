ba54bfc2d526e4e607e7a8207ad805f7cf20acbb
Ajit Waman <ajitwaman353@gamil.com>
2024-05-01 01:05:55 +0530

    cloudinary setup

---
+++ utility/cloudinary.js
@@ -0,0 +1,37 @@
+const cloudinary = require("cloudinary").v2; //import the cloudinary
+const fs = require("fs");
+const dotenv = require("dotenv");
+const joi = require("joi");
+dotenv.config();
+
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+
+// console.log(process.env.CLOUD_NAME);
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+
+// const upload_cloudinary = async (localPath) => {
+//   try {
+//     if (!localPath) return null;
+//     const response = await cloudinary.uploader.upload(localPath, {
+//       folder: "StayHome_images",
+//       resource_type: "auto",
+//     });
+//     console.log(response);
+//   } catch (error) {
+//     fs.unlinkSync(localPath);
+//     console.error("Error uploading file to Cloudinary:", error);
+//   }
+// };
+const cloudinary_for_storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "StayHome_images",
+    resource_type: "auto",
+  },
+});
+// console.log(cloudinary_for_storage);
+module.exports = cloudinary_for_storage;
