c5ed88a11db623094d1a5ac23cf785dcf762799d
Ajit Waman <ajitwaman353@gamil.com>
2024-05-31 11:54:45 +0530

    user alredy email persent or not checked

--- controller/userController.js
+++ controller/userController.js
@@ -4,7 +4,15 @@ const NewUserCrete = async (req, res) => {
   let { username, email, password } = req.body;
 
   try {
+    // check alredy persent or not
+    const exiting_user_email = await User_model.find({ email: email });
+    if (exiting_user_email) {
+      req.flash("error", "Email already in use");
+      return res.redirect("/signUp");
+    }
+    // new user create using username and email
     const newUser = new User_model({ username, email });
+
     const register_User = await User_model.register(newUser, password);
     console.log("register user", register_User);
     req.login(register_User, function (err) {
@@ -15,7 +23,7 @@ const NewUserCrete = async (req, res) => {
       res.redirect("/listings");
     });
   } catch (err) {
-    console.error(err);
+    // console.error(err);
     req.flash("error", "An error occurred during registration");
     return res.render("../views/user/signUp.ejs");
   }
