4905d9a9b58ea32a1e284c2623ee223dd2703182
Ajit Waman <ajitwaman353@gamil.com>
2024-07-21 00:37:45 +0530

    config model

--- model/listing.js
+++ model/listing.js
@@ -1,42 +1,39 @@
-// this is data base we used collections name is staysimple  listing is data base schema
-const { types, string } = require("joi");
 const mongoose = require("mongoose");
-// listing model
 require("dotenv").config();
-// console.log(process.env);
-mongoose
-  .connect(process.env.MongodbAtlas, {})
-  .then(() => console.log("Database connection established"))
-  .catch((error) => console.error("Database connection error:", error));
 
-const ListingSchema = mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+// Define the Listing Schema
+const ListingSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    image: {
+      Url: String,
+      fileName: String,
+    },
+    price: {
+      type: Number,
+    },
+    location: {
+      type: String,
+    },
+    country: {
+      type: String,
+    },
+    Reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: "Review" }],
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
   },
-  description: {
-    type: String,
-  },
-  image: {
-    Url: String,
-    fileName: String,
-  },
-  price: {
-    type: Number,
-  },
-  location: {
-    type: String,
-  },
-  country: {
-    type: String,
-  },
-  Reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: "Review" }],
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
+  { timestamps: true }
+);
 
-    ref: "User", //this is user schema name
-  },
-});
+// Create the model model is listing
+const Listing = mongoose.model("Listing", ListingSchema);
 
-const Listing = mongoose.model("Listing", ListingSchema); //schema
 module.exports = Listing;
