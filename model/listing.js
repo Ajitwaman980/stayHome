75be111a8d39ac2deda260577ed84b8ed47390fe
Ajit Waman <ajitwaman353@gamil.com>
2024-08-04 00:10:15 +0530

    update database add new fileds

--- model/listing.js
+++ model/listing.js
@@ -1,39 +1,52 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-// Define the Listing Schema
-const ListingSchema = mongoose.Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-    },
-    image: {
-      Url: String,
-      fileName: String,
-    },
-    price: {
-      type: Number,
-    },
-    location: {
-      type: String,
-    },
-    country: {
-      type: String,
-    },
-    Reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: "Review" }],
-    owner: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
+const ListingSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
   },
-  { timestamps: true }
-);
+  description: {
+    type: String,
+  },
+  image: {
+    Url: String,
+    fileName: String,
+  },
+  price: {
+    type: Number,
+  },
+  location: {
+    type: String,
+  },
+  country: {
+    type: String,
+  },
+  Reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: "Review" }],
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  bed: {
+    type: Number,
+    min: 0,
+  },
+  bathroom: {
+    type: Number,
+    min: 0,
+  },
+  areaHousewidth: {
+    type: Number,
+ 
+    min: 0,
+  },
+  areaHouseheight: {
+    type: Number,
+   
+    min: 0,
+  },
+}, { timestamps: true });
 
-// Create the model model is listing
 const Listing = mongoose.model("Listing", ListingSchema);
 
 module.exports = Listing;
