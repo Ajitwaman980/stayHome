9c700d4f9c099861a5356f7fcca0a4b0d0c657b6
Ajit Waman <ajitwaman353@gamil.com>
2024-09-08 22:20:37 +0530

    category add api

---
+++ routes/category.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const Listing = require("../model/listing"); //Listing is model
+const { times } = require("lodash");
+router.get('/cat/new', async function (req, res){
+    try {
+        const newdata=await Listing.find().sort({createdAt: -1 });
+         console.log(newdata);
+         res.json({newdata: newdata});
+        // res.render",{newdata:newdata})
+    } catch (error) {
+        // console.log(error);
+        res.send({error: error});
+        
+    }
+
+});
+router.get('/cat/rent', function (req, res){
+
+});
+router.get('/cat/sell', function (req, res){
+
+});
+module.exports = router;
