81c3430f25e2b62f04adf8cb1f2719868e93ce68
Ajit Waman <ajitwaman353@gamil.com>
2024-09-10 05:52:04 +0530

    axios used to fetching the data for soting bar

--- routes/category.js
+++ routes/category.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const Listing = require("../model/listing"); //Listing is model
 const { times } = require("lodash");
 router.get('/cat/new', async function (req, res){
+
     try {
         const newdata=await Listing.find().sort({createdAt: -1 });
-         console.log(newdata);
-         res.json({newdata: newdata});
+        //  console.log(newdata);
+         res.json( newdata);
         // res.render",{newdata:newdata})
     } catch (error) {
         // console.log(error);
