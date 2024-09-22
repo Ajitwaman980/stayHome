6c2417afd526771febd583d756acf041f9021427
ajit waman <ajitwaman353@gmail.com>
2024-09-22 21:52:44 +0530

    update security

--- public/javascripts/category_new_data.js
+++ public/javascripts/category_new_data.js
@@ -1,3 +1,5 @@
+const Listing = require("../../model/listing");
+
 async function navigateToPage() {
   const dropdown = document.getElementById("sort");
   const selectedValue = dropdown.value;
@@ -8,9 +10,12 @@ async function navigateToPage() {
     // Fetch listings based on selected category and sort order
     // console.log(selectedValue)
 
+
     const response = await axios.get(`/listings/category/cat/${selectedValue}`);
 
+
     const listings = response.data;
+    
     const cardContainer = document.getElementById("cardmain");
     let cardsHtml = '<div class="flex w-full flex-wrap justify-center gap-4 ">'; //main card
 
@@ -18,11 +23,19 @@ async function navigateToPage() {
       listings.forEach((listing) => {
         // Calculate discounted price
         let basePrice;
+        
+        if(listing.typeofhouse === "sell"){
+          basePrice = listing.price;
+          // console.log("Base price: " + basePrice)
+        }
+        else{
         if (listing.price > 2000) {
           basePrice = listing.price - 2000;
-        } else {
+        } 
+        else {
           basePrice = listing.price - 100;
         }
+      }
 
         cardsHtml =
           cardsHtml +
@@ -31,13 +44,15 @@ async function navigateToPage() {
             <a href="/listings/${listing._id}" class="block rounded-lg  px-2 shadow-lg overflow-hidden" style="height: 26rem; width:20.2rem;">
             <img src="${listing.image.Url}" class="w-full h-60  py-1 object-cover " alt="image">
              <div class="p-1 px-2 py-3 bg-zinc-200 h-full w-full">
-             <p class="text-black text-xl font-bold mb-1">
-            ${basePrice} &#8377;
-            <span class="text-sm text-gray-500 inline-block" style="margin: -7px;">/Night</span>
-            <span class="text-gray-500 items-end px-8 line-through text-lg font-normal justify-end inline-block">
-                <span>${listing.price} &#8377;<span>Night</span></span>
-            </span>
-            </p>
+            <p class="text-black text-xl font-bold mb-1">
+                                ${basePrice} &#8377;
+                                ${listing.typeofhouse === "rent" ? 
+                                    `<span class="text-sm text-gray-500 inline-block" style="margin: -7px;">/Night</span>` 
+                                    : ''}
+                                <span class="text-gray-500 items-end px-8 line-through text-lg font-normal justify-end inline-block">
+                                    <span>${listing.price} &#8377;<span>Night</span></span>
+                                </span>
+                            </p>
             <p class="text-xl text-wrap text-black font-bold">${listing.title}</p>
             <p class="text-base text-gray-600 py-2 flex items-center">
                                                 <i class="fa-solid fa-map-marker-alt text-red-500 mr-2"></i>
