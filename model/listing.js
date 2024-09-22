6d27a0d3c6e11df97bdc7573afd3787ff16ef6fe
ajit waman <ajitwaman353@gmail.com>
2024-09-22 21:52:10 +0530

    update security

--- model/listing.js
+++ model/listing.js
@@ -2,61 +2,64 @@ const { types } = require("joi");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const ListingSchema = mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const ListingSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    image: {
+      Url: String,
+      fileName: String,
+    },
+    price: {
+      type: Number,
+    },
+    location: {
+      type: String,
+    },
+    country: {
+      type: String,
+    },
+    Reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: "Review" }],
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    bed: {
+      type: Number,
+      min: 0,
+    },
+    bathroom: {
+      type: Number,
+      min: 0,
+    },
+    // type add sell or rent items
+    typeofhouse: {
+      type: String,
+      require: true,
+    },
+
+    areaHousewidth: {
+      type: Number,
+
+      min: 0,
+    },
+    areaHouseheight: {
+      type: Number,
+
+      min: 0,
+    },
+    categories: {
+      type: [String],
+      default: ["Home"],
+    },
   },
-  description: {
-    type: String,
-  },
-  image: {
-    Url: String,
-    fileName: String,
-  },
-  price: {
-    type: Number,
-  },
-  location: {
-    type: String,
-  },
-  country: {
-    type: String,
-  },
-  Reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: "Review" }],
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-  },
-  bed: {
-    type: Number,
-    min: 0,
-  },
-  bathroom: {
-    type: Number,
-    min: 0,
-  },
-  // type add sell or rent items
-  typeofhouse:{
-  type:String,
-  require:true,
-  },
-  
-  areaHousewidth: {
-    type: Number,
- 
-    min: 0,
-  },
-  areaHouseheight: {
-    type: Number,
-   
-    min: 0,
-  },
-  categories:{    
-    type:[String],
-    default: ["Home"],
-  },
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 const Listing = mongoose.model("Listing", ListingSchema);
 
