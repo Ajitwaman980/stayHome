396a8838892976b5613c5bf271562fd44edbc0c0
ajit waman <ajitwaman353@gmail.com>
2024-09-30 22:33:22 +0530

    config changes

--- utility/cloudinary.js
+++ utility/cloudinary.js
@@ -6,12 +6,12 @@ dotenv.config();
 
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
-// console.log(process.env.CLOUD_NAME);
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
-});
+// // console.log(process.env.CLOUD_NAME);
+// cloudinary.config({
+//   cloud_name: process.env.CLOUD_NAME,
+//   api_key: process.env.API_KEY,
+//   api_secret: process.env.API_SECRET,
+// });
 
 // const upload_cloudinary = async (localPath) => {
 //   try {
@@ -26,7 +26,7 @@ cloudinary.config({
 //     console.error("Error uploading file to Cloudinary:", error);
 //   }
 // };
-const  cloudinary_for_storage = new CloudinaryStorage({
+const cloudinary_for_storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: "StayHome_images",
