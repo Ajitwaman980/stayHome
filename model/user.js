f93265a0558b83cb8b8cc685871f91410901498e
ajit waman <ajitwaman353@gmail.com>
2024-10-01 22:16:08 +0530

    update

--- model/user.js
+++ model/user.js
@@ -1,27 +1,26 @@
 const mongoose = require("mongoose");
-const passport_local_mongoose=require("passport-local-mongoose");
-const User_Schema=new mongoose.Schema({
-    email:{
-        type:'String',
-        required:true,
-        unique:true
-    } ,
-    // discoun for new user 
-    discount:{
-        type:Boolean,//true or flase
-        default:true
+const passport_local_mongoose = require("passport-local-mongoose");
+const User_Schema = new mongoose.Schema({
+  email: {
+    type: "String",
+    required: true,
+    unique: true,
+  },
+  // discoun for new user
+  // discount:{
+  //     type:Boolean,//true or flase
+  //     default:true
 
-    },
-    discountCode:{
-type: String,
-    default: null,
-    },
-    discountUsed:{
-        type:Boolean,//true or flase
-        default:false,
+  // },
+  // discountCode:{
+  // type: String,
+  // default: null,
+  // },
+  // discountUsed:{
+  //     type:Boolean,//true or flase
+  //     default:false,
 
-    }
-    
+  // }
 });
 User_Schema.plugin(passport_local_mongoose);
 module.exports = mongoose.model("User", User_Schema);
