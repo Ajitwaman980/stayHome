09ccb233c774f383f5e549ce36af6ac2b295cb26
ajit waman <ajitwaman353@gmail.com>
2024-10-01 22:16:50 +0530

    add verify and send code setup

--- controller/userController.js
+++ controller/userController.js
@@ -1,54 +1,103 @@
 const User_model = require("../model/user");
 const passport = require("passport");
 const statusCodes = require("../utility/statuscoded.js");
-const NewUserCrete = async (req, res, next) => {
-  let { username, email, password } = req.body;
+const sendVerificationEmail = require("../utility/sendmail.js");
+
+//crete new user
+const sendVerificationCode = async (req, res) => {
+  const { username, email, password } = req.body;
 
   try {
-    // Check if the email is already in use
     const existing_user_email = await User_model.findOne({ email });
     if (existing_user_email) {
       req.flash("error", "Email already in use");
       return res.status(statusCodes.OK).redirect("/signUp");
     }
-
-    // Generate a discount code
-    const discountCode = Math.floor(Math.random() * 10000);
-
-    // Create new user with username, email, and discount code
-    const newUser = new User_model({
+    // verification code
+    const verification_code = Math.floor(1000 + Math.random() * 9000);
+    // Store the username, email, password, and verification code in the session
+    req.session.tempUser = {
       username,
       email,
-      discountCode, // Unique discount code for new user
-      discount: true, // Assuming "discount" is a field to track discount eligibility
-    });
+      password,
+      code: verification_code,
+    };
+    // console.log("this is sessions", req.session.tempUser);
 
-    // Register user with passport
-    const register_User = await User_model.register(newUser, password);
-    console.log("Registered user:", register_User);
+    // Send the verification code via email
+    await sendVerificationEmail(email, verification_code);
 
-    // Automatically log in the new user
-    req.login(register_User, function (err) {
-      if (err) {
-        return next(err); // Pass error to Express error handler
-      }
+    req.flash(
+      "success",
+      "Verification email sent successfully. Please enter the code to continue."
+    );
+    return res.status(statusCodes.OK).redirect("/user/verify_code");
+  } catch (err) {
+    console.error("Error during sending verification email:", err);
+    req.flash(
+      "error",
+      "An error occurred while sending the verification code."
+    );
+    return res.status(statusCodes.INTERNAL_SERVER_ERROR).redirect("/signUp");
+  }
+};
+// verify the user
+const verifyCodeAndSignUp = async (req, res) => {
+  // console.log("verifyCodeAndSignUp");
+  let { code } = req.body;
+  // console.log(`Verifying code`, code);
 
-      // Flash success message with discount code
-      req.flash(
-        "success",
-        `User Login Success. You get 50% off on your first booking with code: ${discountCode}`
-      );
+  const {
+    username,
+    email,
+    password,
+    code: verification_code,
+  } = req.session.tempUser;
 
-      // Redirect to listings page after successful login
-      return res.status(statusCodes.OK).redirect("/listings");
-    });
+  if (!username || !email || !password || !verification_code) {
+    req.flash("error", "Session expired. Please try again.");
+    return res.status(statusCodes.OK).redirect("/signUp");
+  }
+  try {
+    // Create new
+    if (code == verification_code) {
+      const newUser = new User_model({
+        username,
+        email,
+      });
+
+      // Register user with passport
+      const register_User = await User_model.register(newUser, password);
+      // console.log("Registered user:", register_User);
+
+      // Automatically log in the new user
+      req.login(register_User, function (err) {
+        if (err) {
+          return next(err);
+        }
+        req.session.tempUser = null;
+        // Flash
+        req.flash("success", `User registered successfully. `);
+
+        // Redirect
+        return res.status(statusCodes.OK).redirect("/listings");
+      });
+    } else {
+      req.flash("error", "Verification code is not correct");
+      return res.status(statusCodes.OK).redirect("/signUp");
+    }
   } catch (err) {
     console.error("Error during registration:", err);
 
-    // Flash error message and re-render sign-up page
+    // Flash error
     req.flash("error", "An error occurred during registration");
-    return res.status(statusCodes.UNAUTHORIZED).render("../views/user/signUp.ejs");
+    return res
+      .status(statusCodes.UNAUTHORIZED)
+      .render("../views/user/signUp.ejs");
   }
 };
 
-module.exports = NewUserCrete;
+module.exports = {
+  sendVerificationCode,
+  verifyCodeAndSignUp,
+};
